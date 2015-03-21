71af6b93c4bf4d67ac7060ec71dda7babd555613
Ruben Tarin <kabum42@gmail.com>
2015-03-21 02:13:25 +0100

    Languages checkpoint

--- core/src/net/piropanda/tinygod/Lang.java
+++ core/src/net/piropanda/tinygod/Lang.java
@@ -1,5 +1,6 @@
 package net.piropanda.tinygod;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 
 public class Lang {
@@ -11,14 +12,272 @@ public class Lang {
 	public static int ENGLISH_VALUE = 0;
 	public static int SPANISH_VALUE = 1;
 	
-	public static String[][]LANGUAGES = new String[15][];
+	public static String[][]LANGUAGES = new String[10][];
 	
-	public static String[]ENGLISH_WORDS = new String[20];
-	public static String[]SPANISH_WORDS = new String[20];
+	public static String[]ENGLISH_WORDS = new String[300];
+	public static String[]SPANISH_WORDS = new String[300];
 	
-	public static int SERVANT_TEXT = 0;
-	public static int HUMAN_TEXT = 1;
-
+	// MISCELLANEOUS
+	public static int TRANSLATOR_MALE = 				0;
+	public static int TRANSLATOR_FEMALE = 				1;
+	public static int TRANSLATORS = 					2;
+	public static int BUILDING = 						3;
+	public static int BUILDINGS = 						4;
+	public static int UPGRADE = 						5;
+	public static int UPGRADES = 						6;
+	public static int ACHIEVEMENT = 					7;
+	public static int ACHIEVEMENTS = 					8;
+	public static int LOVE_SINCE_BEGINNING_GAME = 		9;
+	public static int LOVE_SINCE_BEGINNING_TIMES = 		10;
+	public static int PRODUCTION_MULTIPLIER = 			11;
+	public static int CURRENT_LOVE = 					12;
+	public static int LOVE_PER_CLICK = 					13;
+	public static int CRITICAL_CLICK = 					14;
+	public static int CRITICAL_CLICK_CHANCE = 			15;
+	public static int NUMBER_OF_CLICKS = 				16;
+	public static int NUMBER_OF_MIRACLES = 				17;
+	public static int GAIN_BASE_LOVE = 					18;
+	public static int PRODUCE_TWICE_LOVE = 				19;
+	
+	// SERVANT
+	public static int SERVANT_NAME = 					20;
+	public static int SERVANT_DESCRIPTION = 			21;
+	public static int SERVANT_UPGRADE_1 = 				22;
+	public static int SERVANT_UPGRADE_1_JOKE =  		23;
+	public static int SERVANT_UPGRADE_2 = 				24;
+	public static int SERVANT_UPGRADE_2_JOKE =  		25;
+	public static int SERVANT_UPGRADE_3 = 				26;
+	public static int SERVANT_UPGRADE_3_JOKE =  		27;
+	public static int SERVANT_UPGRADE_4 = 				28;
+	public static int SERVANT_UPGRADE_4_JOKE =  		29;
+	public static int SERVANT_UPGRADE_5 = 				30;
+	public static int SERVANT_UPGRADE_5_JOKE =  		31;
+	public static int SERVANT_UPGRADE_6 = 				32;
+	public static int SERVANT_UPGRADE_6_JOKE =  		33;
+	public static int SERVANT_UPGRADE_7 = 				34;
+	public static int SERVANT_UPGRADE_7_JOKE =  		35;
+	public static int SERVANT_UPGRADE_8 = 				36;
+	public static int SERVANT_UPGRADE_8_JOKE =  		37;
+	public static int SERVANT_UPGRADE_9 = 				38;
+	public static int SERVANT_UPGRADE_9_JOKE =  		39;
+	public static int SERVANT_UPGRADE_10 = 				40;
+	public static int SERVANT_UPGRADE_10_JOKE = 		41;
+	
+	// HUMAN
+	public static int HUMAN_NAME = 						42;
+	public static int HUMAN_DESCRIPTION = 				43;
+	public static int HUMAN_UPGRADE_1 = 				44;
+	public static int HUMAN_UPGRADE_1_JOKE =  			45;
+	public static int HUMAN_UPGRADE_2 = 				46;
+	public static int HUMAN_UPGRADE_2_JOKE =  			47;
+	public static int HUMAN_UPGRADE_3 = 				48;
+	public static int HUMAN_UPGRADE_3_JOKE =  			49;
+	public static int HUMAN_UPGRADE_4 = 				50;
+	public static int HUMAN_UPGRADE_4_JOKE =  			51;
+	public static int HUMAN_UPGRADE_5 = 				52;
+	public static int HUMAN_UPGRADE_5_JOKE =  			53;
+	public static int HUMAN_UPGRADE_6 = 				54;
+	public static int HUMAN_UPGRADE_6_JOKE =  			55;
+	public static int HUMAN_UPGRADE_7 = 				56;
+	public static int HUMAN_UPGRADE_7_JOKE =  			57;
+	public static int HUMAN_UPGRADE_8 = 				58;
+	public static int HUMAN_UPGRADE_8_JOKE =  			59;
+	public static int HUMAN_UPGRADE_9 = 				60;
+	public static int HUMAN_UPGRADE_9_JOKE = 			61;
+	public static int HUMAN_UPGRADE_10 = 				62;
+	public static int HUMANT_UPGRADE_10_JOKE =  		63;
+	
+	// SPECIAL_HUMANS
+	public static int SPECIAL_HUMAN_1 = 				64;
+	public static int SPECIAL_HUMAN_1_JOKE =  			65;
+	public static int SPECIAL_HUMAN_2 = 				66;
+	public static int SPECIAL_HUMAN_2_JOKE =  			67;
+	public static int SPECIAL_HUMAN_3 = 				68;
+	public static int SPECIAL_HUMAN_3_JOKE =  			69;
+	public static int SPECIAL_HUMAN_4 = 				70;
+	public static int SPECIAL_HUMAN_4_JOKE =  			71;
+	public static int SPECIAL_HUMAN_5 = 				72;
+	public static int SPECIAL_HUMAN_5_JOKE =  			73;
+	public static int SPECIAL_HUMAN_6 = 				74;
+	public static int SPECIAL_HUMAN_6_JOKE =  			75;
+	public static int SPECIAL_HUMAN_7 = 				76;
+	public static int SPECIAL_HUMAN_7_JOKE =  			77;
+	public static int SPECIAL_HUMAN_8 = 				78;
+	public static int SPECIAL_HUMAN_8_JOKE =  			79;
+	public static int SPECIAL_HUMAN_9 = 				80;
+	public static int SPECIAL_HUMAN_9_JOKE =  			81;
+	public static int SPECIAL_HUMAN_10 = 				82;
+	public static int SPECIAL_HUMAN_10_JOKE =  			83;
+	
+	// PROPHET
+	public static int PROPHET_NAME = 					84;
+	public static int PROPHET_DESCRIPTION = 			85;
+	public static int PROPHET_UPGRADE_1 = 				86;
+	public static int PROPHET_UPGRADE_1_JOKE =  		87;
+	public static int PROPHET_UPGRADE_2 = 				88;
+	public static int PROPHET_UPGRADE_2_JOKE =  		89;
+	public static int PROPHET_UPGRADE_3 = 				90;
+	public static int PROPHET_UPGRADE_3_JOKE =  		91;
+	public static int PROPHET_UPGRADE_4 = 				92;
+	public static int PROPHET_UPGRADE_4_JOKE =  		93;
+	public static int PROPHET_UPGRADE_5 = 				94;
+	public static int PROPHET_UPGRADE_5_JOKE =  		95;
+	public static int PROPHET_UPGRADE_6 = 				96;
+	public static int PROPHET_UPGRADE_6_JOKE =  		97;
+	public static int PROPHET_UPGRADE_7 = 				98;
+	public static int PROPHET_UPGRADE_7_JOKE =  		99;
+	public static int PROPHET_UPGRADE_8 = 				100;
+	public static int PROPHET_UPGRADE_8_JOKE =  		101;
+	public static int PROPHET_UPGRADE_9 = 				102;
+	public static int PROPHET_UPGRADE_9_JOKE = 			103;
+	public static int PROPHET_UPGRADE_10 = 				104;
+	public static int PROPHETT_UPGRADE_10_JOKE =  		105;
+	
+	// TEMPLE
+	public static int TEMPLE_NAME = 					106;
+	public static int TEMPLE_DESCRIPTION = 				107;
+	public static int TEMPLE_UPGRADE_1 = 				108;
+	public static int TEMPLE_UPGRADE_1_JOKE =  			109;
+	public static int TEMPLE_UPGRADE_2 = 				110;
+	public static int TEMPLE_UPGRADE_2_JOKE =  			111;
+	public static int TEMPLE_UPGRADE_3 = 				112;
+	public static int TEMPLE_UPGRADE_3_JOKE =  			113;
+	public static int TEMPLE_UPGRADE_4 = 				114;
+	public static int TEMPLE_UPGRADE_4_JOKE =  			115;
+	public static int TEMPLE_UPGRADE_5 = 				116;
+	public static int TEMPLE_UPGRADE_5_JOKE =  			117;
+	public static int TEMPLE_UPGRADE_6 = 				118;
+	public static int TEMPLE_UPGRADE_6_JOKE =  			119;
+	public static int TEMPLE_UPGRADE_7 = 				120;
+	public static int TEMPLE_UPGRADE_7_JOKE =  			121;
+	public static int TEMPLE_UPGRADE_8 = 				122;
+	public static int TEMPLE_UPGRADE_8_JOKE =  			123;
+	public static int TEMPLE_UPGRADE_9 = 				124;
+	public static int TEMPLE_UPGRADE_9_JOKE = 			125;
+	public static int TEMPLE_UPGRADE_10 = 				126;
+	public static int TEMPLET_UPGRADE_10_JOKE =  		127;
+	
+	// SHIP
+	public static int SHIP_NAME = 						128;
+	public static int SHIP_DESCRIPTION = 				129;
+	public static int SHIP_UPGRADE_1 = 					130;
+	public static int SHIP_UPGRADE_1_JOKE =  			131;
+	public static int SHIP_UPGRADE_2 = 					132;
+	public static int SHIP_UPGRADE_2_JOKE =  			133;
+	public static int SHIP_UPGRADE_3 = 					134;
+	public static int SHIP_UPGRADE_3_JOKE =  			135;
+	public static int SHIP_UPGRADE_4 = 					136;
+	public static int SHIP_UPGRADE_4_JOKE =  			137;
+	public static int SHIP_UPGRADE_5 = 					138;
+	public static int SHIP_UPGRADE_5_JOKE =  			139;
+	public static int SHIP_UPGRADE_6 = 					140;
+	public static int SHIP_UPGRADE_6_JOKE =  			141;
+	public static int SHIP_UPGRADE_7 = 					142;
+	public static int SHIP_UPGRADE_7_JOKE =  			143;
+	public static int SHIP_UPGRADE_8 = 					144;
+	public static int SHIP_UPGRADE_8_JOKE =  			145;
+	public static int SHIP_UPGRADE_9 = 					146;
+	public static int SHIP_UPGRADE_9_JOKE = 			147;
+	public static int SHIP_UPGRADE_10 = 				148;
+	public static int SHIPT_UPGRADE_10_JOKE =  			149;
+	
+	// FACTORY
+	public static int FACTORY_NAME = 					150;
+	public static int FACTORY_DESCRIPTION = 			151;
+	public static int FACTORY_UPGRADE_1 = 				152;
+	public static int FACTORY_UPGRADE_1_JOKE =  		153;
+	public static int FACTORY_UPGRADE_2 = 				154;
+	public static int FACTORY_UPGRADE_2_JOKE =  		155;
+	public static int FACTORY_UPGRADE_3 = 				156;
+	public static int FACTORY_UPGRADE_3_JOKE =  		157;
+	public static int FACTORY_UPGRADE_4 = 				158;
+	public static int FACTORY_UPGRADE_4_JOKE =  		159;
+	public static int FACTORY_UPGRADE_5 = 				160;
+	public static int FACTORY_UPGRADE_5_JOKE =  		161;
+	public static int FACTORY_UPGRADE_6 = 				162;
+	public static int FACTORY_UPGRADE_6_JOKE =  		163;
+	public static int FACTORY_UPGRADE_7 = 				164;
+	public static int FACTORY_UPGRADE_7_JOKE =  		165;
+	public static int FACTORY_UPGRADE_8 = 				166;
+	public static int FACTORY_UPGRADE_8_JOKE =  		167;
+	public static int FACTORY_UPGRADE_9 = 				168;
+	public static int FACTORY_UPGRADE_9_JOKE = 			169;
+	public static int FACTORY_UPGRADE_10 = 				170;
+	public static int FACTORYT_UPGRADE_10_JOKE =  		171;
+	
+	// LABORATORY
+	public static int LABORATORY_NAME = 				172;
+	public static int LABORATORY_DESCRIPTION = 			173;
+	public static int LABORATORY_UPGRADE_1 = 			174;
+	public static int LABORATORY_UPGRADE_1_JOKE =  		175;
+	public static int LABORATORY_UPGRADE_2 = 			176;
+	public static int LABORATORY_UPGRADE_2_JOKE =  		177;
+	public static int LABORATORY_UPGRADE_3 = 			178;
+	public static int LABORATORY_UPGRADE_3_JOKE =  		179;
+	public static int LABORATORY_UPGRADE_4 = 			180;
+	public static int LABORATORY_UPGRADE_4_JOKE =  		181;
+	public static int LABORATORY_UPGRADE_5 = 			182;
+	public static int LABORATORY_UPGRADE_5_JOKE =  		183;
+	public static int LABORATORY_UPGRADE_6 = 			184;
+	public static int LABORATORY_UPGRADE_6_JOKE =  		185;
+	public static int LABORATORY_UPGRADE_7 = 			186;
+	public static int LABORATORY_UPGRADE_7_JOKE =  		187;
+	public static int LABORATORY_UPGRADE_8 = 			188;
+	public static int LABORATORY_UPGRADE_8_JOKE =  		189;
+	public static int LABORATORY_UPGRADE_9 = 			190;
+	public static int LABORATORY_UPGRADE_9_JOKE = 		191;
+	public static int LABORATORY_UPGRADE_10 = 			192;
+	public static int LABORATORYT_UPGRADE_10_JOKE =  	193;
+	
+	// SHOP
+	public static int SHOP_NAME = 						194;
+	public static int SHOP_DESCRIPTION = 				195;
+	public static int SHOP_UPGRADE_1 = 					196;
+	public static int SHOP_UPGRADE_1_JOKE =  			197;
+	public static int SHOP_UPGRADE_2 = 					198;
+	public static int SHOP_UPGRADE_2_JOKE =  			199;
+	public static int SHOP_UPGRADE_3 = 					200;
+	public static int SHOP_UPGRADE_3_JOKE =  			201;
+	public static int SHOP_UPGRADE_4 = 					202;
+	public static int SHOP_UPGRADE_4_JOKE =  			203;
+	public static int SHOP_UPGRADE_5 = 					204;
+	public static int SHOP_UPGRADE_5_JOKE =  			205;
+	public static int SHOP_UPGRADE_6 = 					206;
+	public static int SHOP_UPGRADE_6_JOKE =  			207;
+	public static int SHOP_UPGRADE_7 = 					208;
+	public static int SHOP_UPGRADE_7_JOKE =  			209;
+	public static int SHOP_UPGRADE_8 = 					210;
+	public static int SHOP_UPGRADE_8_JOKE =  			211;
+	public static int SHOP_UPGRADE_9 = 					212;
+	public static int SHOP_UPGRADE_9_JOKE = 			213;
+	public static int SHOP_UPGRADE_10 = 				214;
+	public static int SHOPT_UPGRADE_10_JOKE =  			215;
+	
+	// SPACESHIP
+	public static int SPACESHIP_NAME = 					216;
+	public static int SPACESHIP_DESCRIPTION = 			217;
+	public static int SPACESHIP_UPGRADE_1 = 			218;
+	public static int SPACESHIP_UPGRADE_1_JOKE =  		219;
+	public static int SPACESHIP_UPGRADE_2 = 			220;
+	public static int SPACESHIP_UPGRADE_2_JOKE =  		221;
+	public static int SPACESHIP_UPGRADE_3 = 			222;
+	public static int SPACESHIP_UPGRADE_3_JOKE =  		223;
+	public static int SPACESHIP_UPGRADE_4 = 			224;
+	public static int SPACESHIP_UPGRADE_4_JOKE =  		225;
+	public static int SPACESHIP_UPGRADE_5 = 			226;
+	public static int SPACESHIP_UPGRADE_5_JOKE =  		227;
+	public static int SPACESHIP_UPGRADE_6 = 			228;
+	public static int SPACESHIP_UPGRADE_6_JOKE =  		229;
+	public static int SPACESHIP_UPGRADE_7 = 			230;
+	public static int SPACESHIP_UPGRADE_7_JOKE =  		231;
+	public static int SPACESHIP_UPGRADE_8 = 			232;
+	public static int SPACESHIP_UPGRADE_8_JOKE =  		233;
+	public static int SPACESHIP_UPGRADE_9 = 			234;
+	public static int SPACESHIP_UPGRADE_9_JOKE = 		235;
+	public static int SPACESHIP_UPGRADE_10 = 			236;
+	public static int SPACESHIPT_UPGRADE_10_JOKE =  	237;
+	
+	
 	//private static HashMap<String, Integer> languages = new HashMap<String, Integer>();
 
 	public static void initialize() {
@@ -29,13 +288,13 @@ public class Lang {
 		LANGUAGES[SPANISH_VALUE] = SPANISH_WORDS;
 		
 		//ENGLISH
-		ENGLISH_WORDS[SERVANT_TEXT] = "These are servants";
-		ENGLISH_WORDS[HUMAN_TEXT] = "These are humans";
+		ENGLISH_WORDS[TRANSLATOR_MALE] = "translator";
+		ENGLISH_WORDS[TRANSLATOR_FEMALE] = "translator";
 		
 		
 		//SPANISH
-		SPANISH_WORDS[SERVANT_TEXT] = "Estos son sirvientes";
-		SPANISH_WORDS[HUMAN_TEXT] = "Estos son humanos";
+		SPANISH_WORDS[TRANSLATOR_MALE] = "traductor";
+		SPANISH_WORDS[TRANSLATOR_FEMALE] = "traductora";
 		
 		
 		//END
