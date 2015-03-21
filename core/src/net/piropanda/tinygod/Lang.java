d5f421112ec133282851a6cd1c8216a70965679e
Ruben Tarin <kabum42@gmail.com>
2015-03-21 20:24:52 +0100

    Languages checkpoint 3

--- core/src/net/piropanda/tinygod/Lang.java
+++ core/src/net/piropanda/tinygod/Lang.java
@@ -131,7 +131,7 @@ public class Lang {
 	public static int PROPHET_UPGRADE_9 = 				102;
 	public static int PROPHET_UPGRADE_9_JOKE = 			103;
 	public static int PROPHET_UPGRADE_10 = 				104;
-	public static int PROPHETT_UPGRADE_10_JOKE =  		105;
+	public static int PROPHET_UPGRADE_10_JOKE =  		105;
 	
 	// TEMPLE
 	public static int TEMPLE_NAME = 					106;
@@ -155,7 +155,7 @@ public class Lang {
 	public static int TEMPLE_UPGRADE_9 = 				124;
 	public static int TEMPLE_UPGRADE_9_JOKE = 			125;
 	public static int TEMPLE_UPGRADE_10 = 				126;
-	public static int TEMPLET_UPGRADE_10_JOKE =  		127;
+	public static int TEMPLE_UPGRADE_10_JOKE =  		127;
 	
 	// SHIP
 	public static int SHIP_NAME = 						128;
@@ -179,7 +179,7 @@ public class Lang {
 	public static int SHIP_UPGRADE_9 = 					146;
 	public static int SHIP_UPGRADE_9_JOKE = 			147;
 	public static int SHIP_UPGRADE_10 = 				148;
-	public static int SHIPT_UPGRADE_10_JOKE =  			149;
+	public static int SHIP_UPGRADE_10_JOKE =  			149;
 	
 	// FACTORY
 	public static int FACTORY_NAME = 					150;
@@ -203,7 +203,7 @@ public class Lang {
 	public static int FACTORY_UPGRADE_9 = 				168;
 	public static int FACTORY_UPGRADE_9_JOKE = 			169;
 	public static int FACTORY_UPGRADE_10 = 				170;
-	public static int FACTORYT_UPGRADE_10_JOKE =  		171;
+	public static int FACTORY_UPGRADE_10_JOKE =  		171;
 	
 	// LABORATORY
 	public static int LABORATORY_NAME = 				172;
@@ -227,7 +227,7 @@ public class Lang {
 	public static int LABORATORY_UPGRADE_9 = 			190;
 	public static int LABORATORY_UPGRADE_9_JOKE = 		191;
 	public static int LABORATORY_UPGRADE_10 = 			192;
-	public static int LABORATORYT_UPGRADE_10_JOKE =  	193;
+	public static int LABORATORY_UPGRADE_10_JOKE =  	193;
 	
 	// SHOP
 	public static int SHOP_NAME = 						194;
@@ -251,7 +251,7 @@ public class Lang {
 	public static int SHOP_UPGRADE_9 = 					212;
 	public static int SHOP_UPGRADE_9_JOKE = 			213;
 	public static int SHOP_UPGRADE_10 = 				214;
-	public static int SHOPT_UPGRADE_10_JOKE =  			215;
+	public static int SHOP_UPGRADE_10_JOKE =  			215;
 	
 	// SPACESHIP
 	public static int SPACESHIP_NAME = 					216;
@@ -275,7 +275,7 @@ public class Lang {
 	public static int SPACESHIP_UPGRADE_9 = 			234;
 	public static int SPACESHIP_UPGRADE_9_JOKE = 		235;
 	public static int SPACESHIP_UPGRADE_10 = 			236;
-	public static int SPACESHIPT_UPGRADE_10_JOKE =  	237;
+	public static int SPACESHIP_UPGRADE_10_JOKE =  	237;
 	
 	
 	//private static HashMap<String, Integer> languages = new HashMap<String, Integer>();
@@ -359,7 +359,192 @@ public class Lang {
 		ENGLISH_WORDS[HUMAN_UPGRADE_10] = "Soylent Green";
 		ENGLISH_WORDS[HUMAN_UPGRADE_10_JOKE] = "100% recycled";
 		
-		//SPANISH
+		// SPECIAL HUMANS
+		ENGLISH_WORDS[SPECIAL_HUMAN_1] = "Zealot";
+		ENGLISH_WORDS[SPECIAL_HUMAN_1_JOKE] = "God exists because that's what the book says, and the book is God’s word";
+		ENGLISH_WORDS[SPECIAL_HUMAN_2] = "Crusader";
+		ENGLISH_WORDS[SPECIAL_HUMAN_2_JOKE] = "That’s only a scratch, I still have another arm";
+		ENGLISH_WORDS[SPECIAL_HUMAN_3] = "Slave driver";
+		ENGLISH_WORDS[SPECIAL_HUMAN_3_JOKE] = "We give them permanent jobs and sometimes even food, they can’t complain";
+		ENGLISH_WORDS[SPECIAL_HUMAN_4] = "Bourgeois";
+		ENGLISH_WORDS[SPECIAL_HUMAN_4_JOKE] = "What's mine is mine and what’s yours will soon be mine";
+		ENGLISH_WORDS[SPECIAL_HUMAN_5] = "Mad scientist";
+		ENGLISH_WORDS[SPECIAL_HUMAN_5_JOKE] = "I call it the ‘L.A.S.E.R.’ Mwahahaha";
+		ENGLISH_WORDS[SPECIAL_HUMAN_6] = "Televangelist";
+		ENGLISH_WORDS[SPECIAL_HUMAN_6_JOKE] = "Hallelujah, brother, your bank transfer won’t be in vain";
+		ENGLISH_WORDS[SPECIAL_HUMAN_7] = "Cyborg";
+		ENGLISH_WORDS[SPECIAL_HUMAN_7_JOKE] = "I have… seen things you people wouldn't believe";
+		
+		// PROPHET
+		ENGLISH_WORDS[PROPHET_NAME] = "Prophet";
+		ENGLISH_WORDS[PROPHET_DESCRIPTION] = "A prophet is a human who understands even less, but thinks the exact opposite";
+		ENGLISH_WORDS[PROPHET_UPGRADE_1] = "Fake beard";
+		ENGLISH_WORDS[PROPHET_UPGRADE_1_JOKE] = "Essential for stoning";
+		ENGLISH_WORDS[PROPHET_UPGRADE_2] = "Pessimistic oracle";
+		ENGLISH_WORDS[PROPHET_UPGRADE_2_JOKE] = "I guess you’ll last for two weeks";
+		ENGLISH_WORDS[PROPHET_UPGRADE_3] = "Apocalyptic prophecy";
+		ENGLISH_WORDS[PROPHET_UPGRADE_3_JOKE] = "And when the seventh seal is opened she will say: ...We need to talk";
+		ENGLISH_WORDS[PROPHET_UPGRADE_4] = "The Flood";
+		ENGLISH_WORDS[PROPHET_UPGRADE_4_JOKE] = "Save two of each kind: two brunettes, two redheads and two blondes";
+		ENGLISH_WORDS[PROPHET_UPGRADE_5] = "Shooting star";
+		ENGLISH_WORDS[PROPHET_UPGRADE_5_JOKE] = "The moment is coming";
+		ENGLISH_WORDS[PROPHET_UPGRADE_6] = "Miraculous duplication";
+		ENGLISH_WORDS[PROPHET_UPGRADE_6_JOKE] = "This is against thermodynam-SHUT UP!";
+		ENGLISH_WORDS[PROPHET_UPGRADE_7] = "Resurrection";
+		ENGLISH_WORDS[PROPHET_UPGRADE_7_JOKE] = "You may take a shower after three days being dead";
+		ENGLISH_WORDS[PROPHET_UPGRADE_8] = "Holy book of Love";
+		ENGLISH_WORDS[PROPHET_UPGRADE_8_JOKE] = "Wait, what are all those positions for?";
+		ENGLISH_WORDS[PROPHET_UPGRADE_9] = "Lovelogy";
+		ENGLISH_WORDS[PROPHET_UPGRADE_9_JOKE] = "All you need to know to avoid it";
+		ENGLISH_WORDS[PROPHET_UPGRADE_10] = "Unfair competition";
+		ENGLISH_WORDS[PROPHET_UPGRADE_10_JOKE] = "If their god were real, wouldn’t he saved them?";
+		
+		// TEMPLE
+		ENGLISH_WORDS[TEMPLE_NAME] = "Temple";
+		ENGLISH_WORDS[TEMPLE_DESCRIPTION] = "A temple in the name of love, more or less";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_1] = "Love Sanctuary";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_1_JOKE] = "A place to give and receive";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_2] = "Generous almsgiving";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_2_JOKE] = "It never has been easier to earn a living";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_3] = "Divine visions";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_3_JOKE] = "No more cheap wine";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_4] = "Obedient altar boys";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_4_JOKE] = "These kids make more bearable the long and hard... masses at the monastery";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_5] = "Remorseful sinners";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_5_JOKE] = "Get on your knees and earn your absolution";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_6] = "Elder in a dress";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_6_JOKE] = "I am the voice of God on earth";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_7] = "Witch hunt";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_7_JOKE] = "These ladies need affection desperately";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_8] = "Punish heretics";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_8_JOKE] = "You have to spank them gently";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_9] = "Repulsive relic";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_9_JOKE] = "I hope that's a wrinkled thumb...";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_10] = "Violent pilgrimage";
+		ENGLISH_WORDS[TEMPLE_UPGRADE_10_JOKE] = "We’re going to the Land of Love and will kill whoever stands in our way";
+		
+		// SHIP
+		ENGLISH_WORDS[SHIP_NAME] = "Ship";
+		ENGLISH_WORDS[SHIP_DESCRIPTION] = "A ship to bring love across the seas";
+		ENGLISH_WORDS[SHIP_UPGRADE_1] = "Into the unknown";
+		ENGLISH_WORDS[SHIP_UPGRADE_1_JOKE] = "Honey, I’m going out for tobacco";
+		ENGLISH_WORDS[SHIP_UPGRADE_2] = "Obscene figurehead";
+		ENGLISH_WORDS[SHIP_UPGRADE_2_JOKE] = "The crew looks excited";
+		ENGLISH_WORDS[SHIP_UPGRADE_3] = "Intercultural exchange";
+		ENGLISH_WORDS[SHIP_UPGRADE_3_JOKE] = "Enjoy the Amazons' coconuts";
+		ENGLISH_WORDS[SHIP_UPGRADE_4] = "Contagious love flu";
+		ENGLISH_WORDS[SHIP_UPGRADE_4_JOKE] = "Love is in the air";
+		ENGLISH_WORDS[SHIP_UPGRADE_5] = "The chosen ones";
+		ENGLISH_WORDS[SHIP_UPGRADE_5_JOKE] = "One race to rule them all";
+		ENGLISH_WORDS[SHIP_UPGRADE_6] = "Convert infidels";
+		ENGLISH_WORDS[SHIP_UPGRADE_6_JOKE] = "Teach them how to love properly";
+		ENGLISH_WORDS[SHIP_UPGRADE_7] = "Pleasurable whiplashes";
+		ENGLISH_WORDS[SHIP_UPGRADE_7_JOKE] = "Now with heart-shaped bruises";
+		ENGLISH_WORDS[SHIP_UPGRADE_8] = "Pirates ahoy!";
+		ENGLISH_WORDS[SHIP_UPGRADE_8_JOKE] = "Arr! Stealing love is easier than earning it!";
+		ENGLISH_WORDS[SHIP_UPGRADE_9] = "Paradise map";
+		ENGLISH_WORDS[SHIP_UPGRADE_9_JOKE] = "Ideal to find the G spot,  I mean, to spot the X";
+		ENGLISH_WORDS[SHIP_UPGRADE_10] = "Invincible Fleet";
+		ENGLISH_WORDS[SHIP_UPGRADE_10_JOKE] = "Definitively invincible";
+		
+		// FACTORY
+		ENGLISH_WORDS[FACTORY_NAME] = "Factory";
+		ENGLISH_WORDS[FACTORY_DESCRIPTION] = "A factory to manufacture love";
+		ENGLISH_WORDS[FACTORY_UPGRADE_1] = "Teamwork";
+		ENGLISH_WORDS[FACTORY_UPGRADE_1_JOKE] = "You sweat and I supervise";
+		ENGLISH_WORDS[FACTORY_UPGRADE_2] = "Tiny hands";
+		ENGLISH_WORDS[FACTORY_UPGRADE_2_JOKE] = "Tiny workers to manage even the tiniest details";
+		ENGLISH_WORDS[FACTORY_UPGRADE_3] = "Lowered standards";
+		ENGLISH_WORDS[FACTORY_UPGRADE_3_JOKE] = "We will have to settle for this";
+		ENGLISH_WORDS[FACTORY_UPGRADE_4] = "Ignore nonsense";
+		ENGLISH_WORDS[FACTORY_UPGRADE_4_JOKE] = "Some workers demand sleep";
+		ENGLISH_WORDS[FACTORY_UPGRADE_5] = "Steam engines";
+		ENGLISH_WORDS[FACTORY_UPGRADE_5_JOKE] = "Increased production at the expense of a few accidents";
+		ENGLISH_WORDS[FACTORY_UPGRADE_6] = "Fair rights";
+		ENGLISH_WORDS[FACTORY_UPGRADE_6_JOKE] = "You have the right to shut up";
+		ENGLISH_WORDS[FACTORY_UPGRADE_7] = "Oversupply";
+		ENGLISH_WORDS[FACTORY_UPGRADE_7_JOKE] = "Don't stop me now";
+		ENGLISH_WORDS[FACTORY_UPGRADE_8] = "Toxic gas leaks";
+		ENGLISH_WORDS[FACTORY_UPGRADE_8_JOKE] = "Dead employees will be fired";
+		ENGLISH_WORDS[FACTORY_UPGRADE_9] = "Scab Monkeys";
+		ENGLISH_WORDS[FACTORY_UPGRADE_9_JOKE] = "They demand a lot of bananas, but do not demand any right";
+		ENGLISH_WORDS[FACTORY_UPGRADE_10] = "Acid Rivers";
+		ENGLISH_WORDS[FACTORY_UPGRADE_10_JOKE] = "Catching fish bare-hands it’s easy when they float face up";
+		
+		// LABORATORY
+		ENGLISH_WORDS[LABORATORY_NAME] = "Laboratory";
+		ENGLISH_WORDS[LABORATORY_DESCRIPTION] = "A laboratory to understand the nature of love";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_1] = "First patent";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_1_JOKE] = "A revolutionary device, but it needs a lot of lube";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_2] = "Brilliant minds";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_2_JOKE] = "We invented a lot of things, but now we have to figure what are they for";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_3] = "Robo-maid";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_3_JOKE] = "Scientists are very pleased";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_4] = "Cute explosion";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_4_JOKE] = "Contains an enriched isotope love";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_5] = "General relativity";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_5_JOKE] = "Sometimes ‘no’ means ‘yes";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_6] = "Love fundamental particle";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_6_JOKE] = "The model is perfect, but reality doesn’t fit well in it";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_7] = "Quantum love";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_7_JOKE] = "Entangled hearts";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_8] = "Violation of physical laws";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_8_JOKE] = "Inside this lab nobody will hear you scream";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_9] = "Broken spatiotemporal fabric";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_9_JOKE] = "We are developing a spatiotemporal needle to patch it";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_10] = "LoveOS";
+		ENGLISH_WORDS[LABORATORY_UPGRADE_10_JOKE] = "The answer to your question is 69";
+		
+		// SHOP
+		ENGLISH_WORDS[SHOP_NAME] = "Shop";
+		ENGLISH_WORDS[SHOP_DESCRIPTION] = "A shop where you can buy love at a reasonable price";
+		ENGLISH_WORDS[SHOP_UPGRADE_1] = "Selling affection";
+		ENGLISH_WORDS[SHOP_UPGRADE_1_JOKE] = "Now you can buy your happiness without commitments";
+		ENGLISH_WORDS[SHOP_UPGRADE_2] = "Crazy sales";
+		ENGLISH_WORDS[SHOP_UPGRADE_2_JOKE] = "For that price… gimme seven!";
+		ENGLISH_WORDS[SHOP_UPGRADE_3] = "Planned obsolescence";
+		ENGLISH_WORDS[SHOP_UPGRADE_3_JOKE] = "Our relationship has no future";
+		ENGLISH_WORDS[SHOP_UPGRADE_4] = "Disposable love";
+		ENGLISH_WORDS[SHOP_UPGRADE_4_JOKE] = "Piles of rubbish as far as the eye can see";
+		ENGLISH_WORDS[SHOP_UPGRADE_5] = "Product design";
+		ENGLISH_WORDS[SHOP_UPGRADE_5_JOKE] = "Awesome, now with 87% more packing!";
+		ENGLISH_WORDS[SHOP_UPGRADE_6] = "Invasive product placement";
+		ENGLISH_WORDS[SHOP_UPGRADE_6_JOKE] = "Enjoy incredible ads while you’re dreaming";
+		ENGLISH_WORDS[SHOP_UPGRADE_7] = "Imported love";
+		ENGLISH_WORDS[SHOP_UPGRADE_7_JOKE] = "Developed by the third world for our joy";
+		ENGLISH_WORDS[SHOP_UPGRADE_8] = "Make up new festivity";
+		ENGLISH_WORDS[SHOP_UPGRADE_8_JOKE] = "Consume more, you bastards";
+		ENGLISH_WORDS[SHOP_UPGRADE_9] = "Loveh*";
+		ENGLISH_WORDS[SHOP_UPGRADE_9_JOKE] = "Almost* like the original";
+		ENGLISH_WORDS[SHOP_UPGRADE_10] = "LoveCard®";
+		ENGLISH_WORDS[SHOP_UPGRADE_10_JOKE] = "There are things that love can’t buy, for everything else: LoveCard®";
+		
+		// SPACESHIP
+		ENGLISH_WORDS[SPACESHIP_NAME] = "Spaceship";
+		ENGLISH_WORDS[SPACESHIP_DESCRIPTION] = "A spaceship to spread love to other galaxies";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_1] = "Astronaut dog";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_1_JOKE] = "Ouch, we forgot to give him his space food";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_2] = "Fake moon landing";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_2_JOKE] = "Raising the audience is also a giant leap";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_3] = "Cosmic jet";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_3_JOKE] = "Shake before use";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_4] = "Space junk";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_4_JOKE] = "Although you can throw it far away it’s not gonna disappear";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_5] = "International Space Station";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_5_JOKE] = "Zero gravity opens up new possibilities, we must update the Holy Book of Love";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_6] = "Pacifist colonies";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_6_JOKE] = "Full of leftover hippies";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_7] = "Open up pink hole";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_7_JOKE] = "Crossing it can cause a tear in the space-time";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_8] = "Close Encounters of the Third Kind";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_8_JOKE] = "They are a bit stupid, but we must learn to love them - Signed: The Aliens";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_9] = "Faster than light";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_9_JOKE] = "Hyperspatial jumping is awesome until the day you crash into a star";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_10] = "Galactic Empire";
+		ENGLISH_WORDS[SPACESHIP_UPGRADE_10_JOKE] = "May the love be with you";
+		
+		
+		/** SPANISH **/
 		
 		// MISCELLANEOUS
 		SPANISH_WORDS[TRANSLATOR_MALE] = "Traductor";
